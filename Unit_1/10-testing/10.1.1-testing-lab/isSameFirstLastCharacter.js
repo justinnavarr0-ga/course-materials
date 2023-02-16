5ea4b21c6f2b13d94e84369d995b147cc0ccb9a4
Evan Maloney <evanpmaloney@gmail.com>
2023-02-16 00:46:03 -0800

    git cache cleared

--- Unit_1/10-testing/10.1.1-testing-lab/isSameFirstLastCharacter.js
+++
@@ -1,7 +0,0 @@
-function isSameFirstLastCharacter(str) {
-  return str.charAt(0) === str.charAt(str.length - 1);
-}
-
-console.log(isSameFirstLastCharacter(""));
-
-module.exports = isSameFirstLastCharacter;
--- Unit_3/3-mern-shopping-cart/3.1.1-ga-stock-market-lab/starter-code/ga-stock-market-lab/src/components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default function Header() {
-  return (
-    <header>
-      <h1 className="text-center">GA Stock Exchange</h1>
-    </header>
-  );
-}
