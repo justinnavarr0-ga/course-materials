29baac5f34b437527af880e1fe5caec474025ef0
gonczarM <mattisagumball@gmail.com>
2023-02-28 15:11:36 -0700

    added callback code

--- unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/callback-functions/app.js
+++ unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/callback-functions/app.js
@@ -49,32 +49,37 @@
 // minimizes code and increases flexibility
 const lightSequence = [
     {color: 'red', time: 3000},
-    {color: 'green', time: 2000},
+    {color: 'orange', time: 2000},
     {color: 'yellow', time: 1000}
-  ];
+];
   
 
   // Cache the divs for the lights
 const lightEls = document.querySelectorAll('main > div');
 
+
 // Variable to track the current light
 let curLightIdx = 0;  // Start with red object
 
-function renderLight(cb) {
+function renderLight() {
     // First, turn off all lights
     lightEls.forEach(el => el.style.backgroundColor = 'black');
     // Next, turn on the current light
     lightEls[curLightIdx].style.backgroundColor = lightSequence[curLightIdx].color;
     // Invoke the callback when this light's time has expired
-    setTimeout(cb, lightSequence[curLightIdx].time);
-  }
+    setTimeout(renderLight, lightSequence[curLightIdx].time);
 
-  function renderLightSequence() {
-    renderLight(renderLightSequence);
-    // Increment and reset to zero when 3 is reached
-    curLightIdx = ++curLightIdx % 3;
-  }
+}
+
+function renderLightSequence() {
+    console.log('hello')
+renderLight(renderLightSequence);
+// Increment and reset to zero when 3 is reached
+curLightIdx = ++curLightIdx % 3;
+console.log(++curLightIdx)
+console.log(curLightIdx++)
+}
   
   // Make it start!
-  renderLightSequence();
+renderLightSequence();
   
