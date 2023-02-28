6ce33a9408b3663cb1012375b64613bd01f8f519
gonczarM <mattisagumball@gmail.com>
2023-02-28 15:40:27 -0700

    cleaned up readmes

--- unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/arrow-functions/app.js
+++ unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/arrow-functions/app.js
@@ -21,5 +21,11 @@ const singleExpression = () => console.log('hello')
 // returning objects need statement block wrapped in parenthesis
 let todos = ['Buy milk', 'Mow lawn'];
 
-let todoObjects = todos.map(todo => ({todo: todo, done: false}));
+//inline object return
+let todoObjectsInline = todos.map(todo => ({todo: todo, done: false}));
+
+// code block object return
+let todoObjects = todos.map(todo => {
+    return {todo: todo, done: false}
+});
 console.log(todoObjects)
--- unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/callback-functions/app.js
+++ unit-1/week-2/d6-js-functions-pt2-and-classes/instructor_notes/callback-functions/app.js
@@ -1,55 +1,40 @@
 
-// const colors = ['blue', 'green', 'red']
+const colors = ['blue', 'green', 'red']
 // anon callback
-// colors.forEach(function(color) {console.log(color)});
+colors.forEach(function(color) {console.log(color)});
 
 // named callback
-// const forEachColor = (color) => {
-//     console.log(color)
-// }
+const forEachColor = (color) => {
+    // console.log(color)
+}
 // colors.forEach(forEachColor)
 
-// colors.forEach(function(color, idx) {
-//     console.log(`${idx} - ${color}`);
-// });
+colors.forEach(function(color, idx) {
+    // console.log(`${idx} - ${color}`);
+});
 
-// const cars = [
-//     { make: 'Toyota', yrsOld: 5, mileage: 92399 },
-//     { make: 'Ford', yrsOld: 12, mileage: 255005 },
-//     { make: 'Ferrari', yrsOld: 9, mileage: 12966 },
-//     { make: 'Subaru', yrsOld: 9, mileage: 111266 },
-//     { make: 'Toyota', yrsOld: 2, mileage: 41888 },
-//     { make: 'Tesla', yrsOld: 3, mileage: 57720 }
-// ];
+const cars = [
+    { make: 'Toyota', yrsOld: 5, mileage: 92399 },
+    { make: 'Ford', yrsOld: 12, mileage: 255005 },
+    { make: 'Ferrari', yrsOld: 9, mileage: 12966 },
+    { make: 'Subaru', yrsOld: 9, mileage: 111266 },
+    { make: 'Toyota', yrsOld: 2, mileage: 41888 },
+    { make: 'Tesla', yrsOld: 3, mileage: 57720 }
+];
 
-// const filterCars = (car) => {
-//     if(car.mileage / car.yrsOld > 20000 ){
-//         return true
-//     }
-// }
+const filterCars = (car) => {
+    if(car.mileage / car.yrsOld > 20000 ){
+        return true
+    }
+}
 
-// const wellDrivenCars = cars.filter(filterCars)
-// wellDrivenCars.forEach(car => console.log(car))
+const wellDrivenCars = cars.filter(filterCars)
+wellDrivenCars.forEach(car => console.log(car))
 
 
-// const colors = ['red', 'green', 'blue'];
-
-// console.log('Code BEFORE the forEach...');
-
-// // setTimeout accepts a callback & how long to wait before calling the cb
-// setTimeout(function() {
-//   colors.forEach(function(color, idx) {
-//     console.log(`${idx + 1} - ${color}`);
-//   });
-// }, 1000);  // 1000 milliseconds (1 sec)
-
-// console.log('Code AFTER the forEach...');
-
-// Using a "lookup" data-structure
-// minimizes code and increases flexibility
 const lightSequence = [
-    {color: 'red', time: 3000},
-    {color: 'orange', time: 2000},
+    {color: 'red', time: 1000},
+    {color: 'orange', time: 1000},
     {color: 'yellow', time: 1000}
 ];
   
@@ -61,23 +46,21 @@ const lightEls = document.querySelectorAll('main > div');
 // Variable to track the current light
 let curLightIdx = 0;  // Start with red object
 
-function renderLight() {
-    // First, turn off all lights
-    lightEls.forEach(el => el.style.backgroundColor = 'black');
-    // Next, turn on the current light
-    lightEls[curLightIdx].style.backgroundColor = lightSequence[curLightIdx].color;
-    // Invoke the callback when this light's time has expired
-    setTimeout(renderLight, lightSequence[curLightIdx].time);
-
+function renderLight(cb) {
+  // First, turn off all lights
+  lightEls.forEach(el => el.style.backgroundColor = 'black');
+  // Next, turn on the current light
+  lightEls[curLightIdx].style.backgroundColor = lightSequence[curLightIdx].color;
+  // Invoke the callback when this light's time has expired
+  setTimeout(cb, lightSequence[curLightIdx].time);
 }
 
+
+renderLight()
 function renderLightSequence() {
-    console.log('hello')
 renderLight(renderLightSequence);
 // Increment and reset to zero when 3 is reached
-curLightIdx = ++curLightIdx % 3;
-console.log(++curLightIdx)
-console.log(curLightIdx++)
+  curLightIdx = ++curLightIdx % 3;
 }
   
   // Make it start!
