4a1ba6c70d3ed3635cebddaf033409ceaf0ff5d2
GonczarM <matthew.gonczar@gmail.com>
2023-03-14 13:17:11 -0600

    finished express todos

--- unit-2/week-1/d2-routers-and-controllers/instructor_notes/express-todos/routes/index.js
+++ unit-2/week-1/d2-routers-and-controllers/instructor_notes/express-todos/routes/index.js
@@ -3,12 +3,9 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  console.log('first index hit')
-  next();
-});
-
-router.get('/index', function(req, res, next) {
   res.render('index', { title: 'SEIR BTY' });
 });
+
+
 
 module.exports = router;
