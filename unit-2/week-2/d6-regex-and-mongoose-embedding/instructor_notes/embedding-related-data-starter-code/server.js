46e6c6d2a792278ead614e91e475e60d8d8d6c6f
kennethchang <kenneth.chang@generalassemb.ly>
2023-03-21 03:37:32 +0800

    Can add a review to the database

--- unit-2/week-2/d6-regex-and-mongoose-embedding/instructor_notes/embedding-related-data-starter-code/server.js
+++ unit-2/week-2/d6-regex-and-mongoose-embedding/instructor_notes/embedding-related-data-starter-code/server.js
@@ -10,6 +10,7 @@ require('./config/database');
 
 var indexRouter = require('./routes/index');
 var moviesRouter = require('./routes/movies');
+var reviewsRouter = require('./routes/reviews');
 
 var app = express();
 
@@ -25,6 +26,8 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/movies', moviesRouter);
+// Mounting to root `/` becuase not ALL routes for reviews start with /reviews
+app.use('/', reviewsRouter)
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
