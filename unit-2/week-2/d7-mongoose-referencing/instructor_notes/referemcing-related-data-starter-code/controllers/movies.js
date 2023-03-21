205f20dd4a260c7fe35b08c40eeaa845cf970f5b
GonczarM <matthew.gonczar@gmail.com>
2023-03-21 13:34:19 -0600

    finished referencing data

--- unit-2/week-2/d7-mongoose-referencing/instructor_notes/referemcing-related-data-starter-code/controllers/movies.js
+++ unit-2/week-2/d7-mongoose-referencing/instructor_notes/referemcing-related-data-starter-code/controllers/movies.js
@@ -1,4 +1,5 @@
 const Movie = require('../models/movie');
+const Performer = require('../models/performer');
 
 module.exports = {
   index,
@@ -13,8 +14,15 @@ async function index(req, res) {
 }
 
 async function show(req, res) {
-  const movie = await Movie.findById(req.params.id);
-  res.render('movies/show', { title: 'Movie Detail', movie });
+  const movie = await Movie.findById(req.params.id).populate('cast')
+  console.log(movie.cast)
+  const performers = await Performer.find({_id: {$nin: movie.cast}})
+  // console.log(performers)
+  res.render('movies/show', { 
+    title: 'Movie Detail', 
+    movie,
+    performers
+  });
 }
 
 function newMovie(req, res) {
@@ -27,18 +35,18 @@ async function create(req, res) {
   // convert nowShowing's checkbox of nothing or "on" to boolean
   req.body.nowShowing = !!req.body.nowShowing;
   // remove any whitespace at start and end of cast
-  req.body.cast = req.body.cast.trim();
+  // req.body.cast = req.body.cast.trim();
   // split cast into an array if it's not an empty string - using a regular expression as a separator
-  if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
+  // if (req.body.cast) req.body.cast = req.body.cast.split(/\s*,\s*/);
   // Remove empty properties so that defaults will be applied
   for (let key in req.body) {
     if (req.body[key] === '') delete req.body[key];
   }
   try {
-    await Movie.create(req.body);
+    const movie = await Movie.create(req.body);
     // Always redirect after CUDing data
     // We'll refactor to redirect to the movies index after we implement it
-    res.redirect('/movies');  // Update this line
+    res.redirect(`/movies/${movie._id}`);  // Update this line
   } catch (err) {
     // Typically some sort of validation error
     console.log(err);
