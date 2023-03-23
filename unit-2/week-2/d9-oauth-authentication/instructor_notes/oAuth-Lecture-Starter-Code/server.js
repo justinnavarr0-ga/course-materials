b3c44c9a9c67b20bd1ae86d5c7bdfb97b6d39a7e
kennethchang <kenneth.chang@generalassemb.ly>
2023-03-24 03:06:10 +0800

    finished installing PassportJS

--- unit-2/week-2/d9-oauth-authentication/instructor_notes/oAuth-Lecture-Starter-Code/server.js
+++ unit-2/week-2/d9-oauth-authentication/instructor_notes/oAuth-Lecture-Starter-Code/server.js
@@ -3,6 +3,10 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+// session will create and add a cryptographically signed cookie
+var session = require('express-session');
+var passport = require('passport');
+
 
 require('dotenv').config();
 // connect to the database with AFTER the config vars are processed
@@ -25,6 +29,16 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(session({
+  secret: process.env.SECRET,
+  resave: false,
+  saveUninitialized: true
+}));
+// Mounting Passport.js
+app.use(passport.initialize());
+app.use(passport.session());
+
 
 app.use('/', indexRouter);
 app.use('/movies', moviesRouter);
