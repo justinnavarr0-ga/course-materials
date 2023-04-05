bf61b3f19d5003e4fbb319c5c654f5fd4540feea
GonczarM <matthew.gonczar@gmail.com>
2023-04-05 08:59:37 -0600

    added starter code for d3

---
+++ unit-3/week-1/d3-react-input-and-event-handling/instructor_notes/react-to-do/src/components/ToDoList/ToDoList.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import ToDoListItem from '../ToDoListItem/ToDoListItem';
+import "./ToDoList.css"
+
+export default function ToDoList({ todos }) {
+  // Create an array of <ToDoListItem> components
+  const toDoListItems = todos.map(t => <ToDoListItem />);
+  return (
+    <ul className="ToDoList">
+      {todos.map((t, idx) => <ToDoListItem todo={t} index={idx} key={idx} />)}
+    </ul>
+  );
+}
+
