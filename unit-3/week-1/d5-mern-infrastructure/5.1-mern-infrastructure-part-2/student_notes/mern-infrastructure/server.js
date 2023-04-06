7c88c0f67da8e4f37ff9f4b2de2061b373ec04d5
Matthew Gonczar <mattisagumball@gmail.com>
2023-04-06 14:57:45 -0600

    Update server.js

--- unit-3/week-1/d5-mern-infrastructure/5.1-mern-infrastructure-part-2/student_notes/mern-infrastructure/server.js
+++ unit-3/week-1/d5-mern-infrastructure/5.1-mern-infrastructure-part-2/student_notes/mern-infrastructure/server.js
@@ -9,7 +9,7 @@ app.use(express.json());
 app.use(express.static(path.join(__dirname, 'dist')));
 
 app.get('/*', function(req, res) {
-  res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  res.sendFile(path.join(__dirname, 'dist', 'index.html'));
 });
 
 const port = process.env.PORT || 3000;
