63f8303f35c7873418fa44140027f7e5c55448d1
GonczarM <matthew.gonczar@gmail.com>
2023-04-11 12:08:26 -0600

    finished part 6

--- unit-3/week-2/d7-mern-infrastructure-continued-2/7.1-mern-infrastructure-part-6/instructor_notes/mern-infrastructure/src/pages/App/App.jsx
+++ unit-3/week-2/d7-mern-infrastructure-continued-2/7.1-mern-infrastructure-part-6/instructor_notes/mern-infrastructure/src/pages/App/App.jsx
@@ -5,24 +5,29 @@ import OrderHistoryPage from '../OrderHistoryPage/OrderHistoryPage'
 import AuthPage from '../AuthPage/AuthPage'
 import { Routes, Route } from 'react-router-dom';
 import NavBar from '../../components/NavBar/NavBar'
+import { getUser } from '../../utilities/users-service';
 
 export default function App() {
 
-  const [user, setUser] = useState(null)
+  const [user, setUser] = useState(getUser())
+
+  function updateUser(userState){
+    setUser(userState)
+  }
 
   return (
     <main className="App">
 
       {user ?
         <>
-          <NavBar />
+          <NavBar user={user} updateUser={updateUser}/>
           <Routes>
             <Route path="/orders/" element={<OrderHistoryPage />} />
             <Route path="/orders/new" element={<NewOrderPage />} />
           </Routes>
         </>
         :
-        <AuthPage />
+        <AuthPage setUser={updateUser} />
       }
     </main>
   )
--- unit-3/week-2/d7-mern-infrastructure-continued-2/7.1-mern-infrastructure-part-6/instructor_notes/mern-infrastructure/src/pages/AuthPage/AuthPage.jsx
+++ unit-3/week-2/d7-mern-infrastructure-continued-2/7.1-mern-infrastructure-part-6/instructor_notes/mern-infrastructure/src/pages/AuthPage/AuthPage.jsx
@@ -2,11 +2,11 @@ import React from 'react'
 import SignUpForm from '../../components/SignUpForm/SignUpForm'
 
 
-export default function AuthPage() {
+export default function AuthPage({setUser}) {
   return (
     <>
       <h1>AuthPage</h1>
-      <SignUpForm />
+      <SignUpForm setUser={setUser}/>
     </>
   )
 }
