fa4983a45e313dc1180e22e2385a53b8c8e17c51
pfulcher26 <pfulcher26@gmail.com>
2023-05-02 15:55:13 -0700

    End of day code for unit 4 week 2 day 7

--- unit-4/week-2/d7-django-models/instructor_notes/catcollector/main_app/views.py
+++ unit-4/week-2/d7-django-models/instructor_notes/catcollector/main_app/views.py
@@ -1,10 +1,5 @@
 from django.shortcuts import render
-
-cats = [
-    {'name': 'Lolo', 'breed': 'tabby', 'description': 'furry little demon', 'age': 3},
-    {'name': 'Sachi', 'breed': 'calico',
-        'description': 'gentle and loving', 'age': 2},
-]
+from .models import Cat 
 
 # Create your views here.
 
@@ -21,7 +16,12 @@ def about(request):
 
 
 def cats_index(request):
+    cats = Cat.objects.all()
     # We pass data to a template very much like we did in Express!
     return render(request, 'cats/index.html', {
         'cats': cats
     })
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    return render(request, 'cats/detail.html', { 'cat': cat })
