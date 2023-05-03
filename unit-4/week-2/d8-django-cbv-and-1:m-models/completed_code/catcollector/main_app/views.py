b7afc5085b8c17e0ae73460eb6518839c62fc1bc
GonczarM <matthew.gonczar@gmail.com>
2023-05-03 13:40:03 -0600

    class based views done

---
+++ unit-4/week-2/d8-django-cbv-and-1:m-models/completed_code/catcollector/main_app/views.py
@@ -0,0 +1,57 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Cat 
+from .forms import FeedingForm
+
+# Create your views here.
+
+# define the home view
+
+
+def home(request):
+    # We need a .html file in our templates
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    # We pass data to a template very much like we did in Express!
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat,
+        'feeding_form': feeding_form
+    })
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = '__all__'
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
+
+def add_feeding(request, cat_id):
+  # create a ModelForm instance using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    # has the cat_id assigned
+    new_feeding = form.save(commit=False)
+    new_feeding.cat_id = cat_id
+    new_feeding.save()
+  return redirect('detail', cat_id=cat_id)
--- unit-4/week-2/d8-django-cbv-and-1:m-models/instructor_notes/catcollector/main_app/models.py
+++ unit-4/week-2/d8-django-cbv-and-1:m-models/instructor_notes/catcollector/main_app/models.py
@@ -1,4 +1,11 @@
 from django.db import models
+from django.urls import reverse
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
 
 class Cat(models.Model):
     name = models.CharField(max_length=100)
@@ -8,5 +15,18 @@ class Cat(models.Model):
 
     def __str__(self):
         return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'cat_id': self.id})
 
-# Create your models here.
+class Feeding(models.Model):
+    date = models.DateField()
+    meal = models.CharField(
+        max_length=1,
+        choices=MEALS,
+        default=MEALS[0][0]
+    )
+    cat = models.ForeignKey(Cat, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_meal_display()} on {self.date}"
