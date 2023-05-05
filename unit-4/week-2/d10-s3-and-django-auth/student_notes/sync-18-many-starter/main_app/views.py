074d5c27190159fa4d58e5389743d0ecdddd7a68
GonczarM <matthew.gonczar@gmail.com>
2023-05-05 09:38:50 -0600

    added starter code d10

---
+++ unit-4/week-2/d10-s3-and-django-auth/student_notes/sync-18-many-starter/main_app/views.py
@@ -0,0 +1,105 @@
+from django.shortcuts import render, redirect
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Cat, Toy
+from .forms import FeedingForm
+
+# Create your views here.
+
+
+def home(request):
+    return render(request, 'home.html')
+
+
+def about(request):
+    return render(request, 'about.html')
+
+
+def cats_index(request):
+    cats = Cat.objects.all()
+    return render(request, 'cats/index.html', {
+        'cats': cats
+    })
+
+
+def cats_detail(request, cat_id):
+    cat = Cat.objects.get(id=cat_id)
+
+    # Get some toys the cat does not have
+    # First, let's create a list of the toy ids that the cat DOES have:
+    id_list = cat.toys.all().values_list('id')
+    # Query / search for toys whose ids are not in teh list using EXCLUDE
+    toys_cat_doesnt_have = Toy.objects.exclude(id__in=id_list)
+    # instantiate FeedingForm to be rendered in detail.html
+    feeding_form = FeedingForm()
+    return render(request, 'cats/detail.html', {
+        'cat': cat, 'feeding_form': feeding_form,
+        # Add toys to be displayed:
+        'toys': toys_cat_doesnt_have
+    })
+
+
+class CatCreate(CreateView):
+    model = Cat
+    fields = ['name', 'breed', 'description', 'age']
+
+
+class CatUpdate(UpdateView):
+    model = Cat
+    fields = ['breed', 'description', 'age']
+
+
+class CatDelete(DeleteView):
+    model = Cat
+    success_url = '/cats'
+
+
+def add_feeding(request, cat_id):
+    # create a ModelForm instance using
+    # the data that was submitted in the form
+    form = FeedingForm(request.POST)
+    # validate the form
+    if form.is_valid():
+        # We want a model instance, but
+        # we can't save to the db yet
+        # because we have not assigned the
+        # cat_id FK.
+        new_feeding = form.save(commit=False)
+        new_feeding.cat_id = cat_id
+        new_feeding.save()
+    return redirect('detail', cat_id=cat_id)
+
+
+class ToyList(ListView):
+    model = Toy
+
+
+class ToyDetail(DetailView):
+    model = Toy
+
+
+class ToyCreate(CreateView):
+    model = Toy
+    fields = '__all__'
+
+
+class ToyUpdate(UpdateView):
+    model = Toy
+    fields = ['name', 'color']
+
+
+class ToyDelete(DeleteView):
+    model = Toy
+    success_url = '/toys'
+
+
+def assoc_toy(request, cat_id, toy_id):
+    # You can pass a toy's id instead of the whole toy object
+    Cat.objects.get(id=cat_id).toys.add(toy_id)
+    return redirect('detail', cat_id=cat_id)
+
+
+def unassoc_toy(request, cat_id, toy_id):
+    # You can pass a toy's id instead of the whole toy object
+    Cat.objects.get(id=cat_id).toys.remove(toy_id)
+    return redirect('detail', cat_id=cat_id)
